3f9eac644d73f12a875505f0665e42e7dc59a350
NightSh4dee <minepipe1227@gmail.com>
2025-06-12 13:10:52 -0300

    add arquivos

---
+++ Aula 1/Atividade1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int h = 72;
+    int e = 69;
+    int i = 73;
+    int t = 84;
+    int o = 79;
+    int r = 82;
+
+    printf("%c%c%c%c%c%c\n", h, e, i, t, o, r);
+
+    return 0;
+}
---
+++ Aula 1/Testes/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*int main() {
+    int idade = 77;
+    puts ("Meu Codigo");
+    printf ("Thiago\n");
+    printf ("Minha idade eh %d\n", idade);
+    return 0;
+}
+*/
+
